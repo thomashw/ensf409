bc29732ebbc58f159dca67b99a1c55ef2a373fdd
Andrew Winkler <andrew.winkler@me.com>
2012-03-07 18:28:21 -0700

    Updated testing.cpp with new variable names and to test class Rectangle

--- Lab 7/Part II/testing.cpp
+++ Lab 7/Part II/testing.cpp
@@ -4,39 +4,52 @@ using namespace std;
 #include "Point.h"
 #include "Shape.h"
 #include "Square.h"
+#include "Rectangle.h"
 
 int main( void )
 {
 	cout << "\nTesting..\n";
 
 	// Testing Point
-	Point p( 3, 4 );
+	Point p1( 3, 4 );
 
-	cout << "X is: " << p.getX() << "\n";
-	cout << "Y is: " << p.getY() << "\n";
-	cout << "ID is: " << p.getID() << "\n";
+	cout << "X is: " << p1.getX() << "\n";
+	cout << "Y is: " << p1.getY() << "\n";
+	cout << "ID is: " << p1.getID() << "\n";
 
-	Point q( 2, 6 );
+	Point p2( 2, 6 );
 
-	cout << "X is: " << q.getX() << "\n";
-	cout << "Y is: " << q.getY() << "\n";
-	cout << "ID is: " << q.getID() << "\n";
+	cout << "X is: " << p2.getX() << "\n";
+	cout << "Y is: " << p2.getY() << "\n";
+	cout << "ID is: " << p2.getID() << "\n";
 
-	cout << "Distance is: " << p.distance(q) << "\n";
-	cout << "Number of Points is: " << p.counter() << "\n";
+	cout << "Distance is: " << p1.distance(p2) << "\n";
+	cout << "Number of Points is: " << p1.counter() << "\n";
 
-	p.display();
-	q.display();
+	p1.display();
+	p2.display();
 
 	// Testing Square
-	Square s( "Steve", 2, 9, 3);
-	s.display();
-	Square t("Bob", 1, 1, 5);
-	t.display();
+	Square s1( "Steve", 2, 9, 3 );
+	s1.display();
+	Square s2( "Bob", 1, 1, 5 );
+	s2.display();
 
-	cout << "Distance is: " << s.distance(t) << "\n";
-	cout << "The Area is: " << s.area() << "\n";
-	cout << "The perimeter is: " << s.perimeter() << "\n";
+	cout << "Distance is: " << s1.distance(s2) << "\n";
+	cout << "The Area is: " << s1.area() << "\n";
+	cout << "The perimeter is: " << s1.perimeter() << "\n";
+
+	// Testing Rectangle
+
+	Rectangle r1( "Andrew", 2, 2, 3, 5 );
+	r1.display();
+	Rectangle r2( "Thomas", 4, 4, 5, 7);
+	r2.display();
+
+	cout << "Distance is: " << r1.distance(r2) << "\n";
+	cout << "The Area is: " << r1.area() << "\n";
+	cout << "The perimeter is: " << r1.perimeter() << "\n";
 
 	cout << ".. Done Testing.\n\n";
+
 }
