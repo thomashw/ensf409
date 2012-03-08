9b47721fd8ae2b29710b10c90c4fcfe5e14237d5
Andrew Winkler <andrew.winkler@me.com>
2012-03-07 19:03:50 -0700

    Added main.cpp to call GraphicsWorld. Moved the code in testing.cpp to run() in class GraphicsWorld, deleted testing.cpp, will need to add more tests to GraphicsWorld

--- Lab 7/Part II/GraphicsWorld.cpp
+++ Lab 7/Part II/GraphicsWorld.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 #include "Point.h"
 #include "Shape.h"
-#include "Square.h"
 #include "Rectangle.h"
+#include "Square.h"
+#include "GraphicsWorld.h"
 
-int main( void )
+GraphicsWorld::GraphicsWorld() {}
+
+void GraphicsWorld::run()
 {
-	cout << "\nTesting..\n";
+	cout << "\n** Program Written by: Andrew Winkler & Thomas Hewton-Waters **\n";
 
 	// Testing Point
+	cout << "\nTesting Point:\n";
 	Point p1( 3, 4 );
 
 	cout << "X is: " << p1.getX() << "\n";
@@ -23,33 +27,33 @@ int main( void )
 	cout << "Y is: " << p2.getY() << "\n";
 	cout << "ID is: " << p2.getID() << "\n";
 
-	cout << "Distance is: " << p1.distance(p2) << "\n";
+	cout << "Distance between P1 and P2 is: " << p1.distance(p2) << "\n";
 	cout << "Number of Points is: " << p1.counter() << "\n";
 
 	p1.display();
 	p2.display();
 
 	// Testing Square
+	cout << "\nTesting Square:\n";
 	Square s1( "Steve", 2, 9, 3 );
 	s1.display();
 	Square s2( "Bob", 1, 1, 5 );
 	s2.display();
 
-	cout << "Distance is: " << s1.distance(s2) << "\n";
+	cout << "Distance between S1 and S2 is: " << s1.distance(s2) << "\n";
 	cout << "The Area is: " << s1.area() << "\n";
 	cout << "The perimeter is: " << s1.perimeter() << "\n";
 
 	// Testing Rectangle
-
+	cout << "\nTesting Rectangle:\n";	
 	Rectangle r1( "Andrew", 2, 2, 3, 5 );
 	r1.display();
 	Rectangle r2( "Thomas", 4, 4, 5, 7);
 	r2.display();
 
-	cout << "Distance is: " << r1.distance(r2) << "\n";
+	cout << "Distance between R1 and R2 is: " << r1.distance(r2) << "\n";
 	cout << "The Area is: " << r1.area() << "\n";
 	cout << "The perimeter is: " << r1.perimeter() << "\n";
 
 	cout << ".. Done Testing.\n\n";
-
 }
