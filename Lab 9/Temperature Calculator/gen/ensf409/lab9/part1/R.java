18d42fa34e9200f61be4c4ea5b7a4e1daae7324f
Andrew Winkler <andrew.winkler@me.com>
2012-03-28 23:11:18 -0600

    Added more string constants, adjusted the UI
    Signed-off-by: Andrew Winkler <andrew.winkler@me.com>

--- Lab 9/Temperature Calculator/gen/ensf409/lab9/part1/R.java
+++ Lab 9/Temperature Calculator/gen/ensf409/lab9/part1/R.java
@@ -14,10 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050003;
+        public static final int Button01=0x7f050003;
+        public static final int button1=0x7f050002;
         public static final int tempCvalue=0x7f050000;
-        public static final int tempFvalue=0x7f050001;
-        public static final int tempResult=0x7f050002;
+        public static final int tempResult=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -25,6 +25,8 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int calculateButton=0x7f040004;
+        public static final int calculateFarenheit=0x7f040008;
+        public static final int calcutateCelsius=0x7f040007;
         public static final int celsius=0x7f040005;
         public static final int fahrenheit=0x7f040006;
         public static final int hello=0x7f040000;
