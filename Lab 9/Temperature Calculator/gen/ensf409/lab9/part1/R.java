9d94be9e0526598fb3a17db092d29e4f38f87904
Andrew Winkler <andrew.winkler@me.com>
2012-03-29 14:05:35 -0600

    Modified the UI to LinearLayout, AbsoluteLayout is deprecated. Refined string constants in strings.xml. Linked the Temperature class with the UI
    Signed-off-by: Andrew Winkler <andrew.winkler@me.com>

--- Lab 9/Temperature Calculator/gen/ensf409/lab9/part1/R.java
+++ Lab 9/Temperature Calculator/gen/ensf409/lab9/part1/R.java
@@ -14,23 +14,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Button01=0x7f050003;
-        public static final int button1=0x7f050002;
-        public static final int tempCvalue=0x7f050000;
-        public static final int tempResult=0x7f050001;
+        public static final int calcC=0x7f050004;
+        public static final int calcF=0x7f050003;
+        public static final int tempResult=0x7f050002;
+        public static final int tempUnit=0x7f050001;
+        public static final int tempValue=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int calculateButton=0x7f040004;
-        public static final int calculateFarenheit=0x7f040008;
-        public static final int calcutateCelsius=0x7f040007;
-        public static final int celsius=0x7f040005;
-        public static final int fahrenheit=0x7f040006;
+        public static final int calculateFarenheit=0x7f040005;
+        public static final int calcutateCelsius=0x7f040004;
         public static final int hello=0x7f040000;
         public static final int result=0x7f040003;
         public static final int tempEntry=0x7f040002;
+        public static final int unit=0x7f040006;
     }
 }
--- Lab 9/Temperature Calculator/src/ensf409/lab9/part1/TemperatureCalculatorActivity.java
+++ Lab 9/Temperature Calculator/src/ensf409/lab9/part1/TemperatureCalculatorActivity.java
@@ -1,7 +1,13 @@
 package ensf409.lab9.part1;
 
+import ensf409.lab9.part1.Temperature.TemperatureType;
+import java.lang.String;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TemperatureCalculatorActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,52 @@ public class TemperatureCalculatorActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button calcC = (Button) findViewById(R.id.calcC);
+        Button calcF = (Button) findViewById(R.id.calcF);
+        
+        calcC.setOnClickListener( new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				
+				EditText tempValue = (EditText) findViewById( R.id.tempValue );
+				EditText tempUnit = (EditText) findViewById( R.id.tempUnit );
+				TextView resultValue = (TextView) findViewById( R.id.tempResult );
+				
+				TemperatureType type = null;
+				String userUnit = tempUnit.getText().toString(); 
+				double userTemp = Double.parseDouble( tempValue.getText().toString() );
+				
+				if( userUnit.compareTo("C") == 0 )
+					type = Temperature.TemperatureType.TemperatureTypeCelsius;
+				else if( userUnit.compareTo("F") == 0 )
+					type = Temperature.TemperatureType.TemperatureTypeFahrenheit;
+				
+				Temperature T1 = new Temperature( userTemp, type );
+				resultValue.setText( Double.toString(T1.toCelsius()) );
+			} 
+		});
+        
+        calcF.setOnClickListener( new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				
+				EditText tempValue = (EditText) findViewById( R.id.tempValue );
+				EditText tempUnit = (EditText) findViewById( R.id.tempUnit );
+				TextView resultValue = (TextView) findViewById( R.id.tempResult );
+				
+				TemperatureType type = null;
+				String userUnit = tempUnit.getText().toString(); 
+				double userTemp = Double.parseDouble( tempValue.getText().toString() );
+				
+				if( userUnit.compareTo("C") == 0 )
+					type = Temperature.TemperatureType.TemperatureTypeCelsius;
+				else if( userUnit.compareTo("F") == 0 )
+					type = Temperature.TemperatureType.TemperatureTypeFahrenheit;
+
+				Temperature T1 = new Temperature( userTemp, type );
+				resultValue.setText( Double.toString(T1.toFahrenheit()) );
+			}
+		});
     }
 }
