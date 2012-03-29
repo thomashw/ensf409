926546671b0d7661aef00f9d775ca636a544dc0f
Andrew Winkler <andrew.winkler@me.com>
2012-03-28 22:14:56 -0600

    Started a new project "Temperature Calculator" Added string constants to strings.xml

---
+++ Lab 9/Temperature Calculator/gen/ensf409/lab9/part1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ensf409.lab9.part1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Lab 9/Temperature Calculator/src/ensf409/lab9/part1/TemperatureCalculatorActivity.java
@@ -0,0 +1,13 @@
+package ensf409.lab9.part1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TemperatureCalculatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
